2afe76eaf0162ad35f30fc5dda9c34880f0f8597
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2013-10-12 20:56:39 +0900

    気にしない

--- tokyo/module/reserveHeader.php
+++ tokyo/module/reserveHeader.php
@@ -7,12 +7,13 @@
 	<meta charset="UTF-8" />
 	<?PHP
 		//各ファイル名の取得
-		$filename = basename($_SERVER["PHP_SELF"],".php");
+/* 		$filename = basename($_SERVER["PHP_SELF"],".php"); */
 	?>
 	<link rel='stylesheet' href='../module/css/reserveStyle.css' />
 	<script src="../module/js/jquery-1.9.1.js"></script>
 	<script src="../module/js/jquery-ui-1.10.3.custom.min.js"></script>
 	<?PHP
+/*
 		//jsのファイルパス
 		if(file_exists("../module/js/script.js")){
 			echo "<script src='../module/js/script.js'></script>\n";
@@ -22,8 +23,9 @@
 			echo "<script src='../module/js/".$filename.".js'></script>\n";
 		}
 
+*/
 	?>
-	<title><?PHP if($pageTitle != ""){ echo $pageTitle." | "; } ?>HALCinema</title>
+	<title><?PHP /* if($pageTitle != ""){ echo $pageTitle." | "; } */ ?>HALCinema</title>
 </head>
 <body>
 <header>
