b6b2baa72a6b8f62788b2dd15f1053f4327d5b01
SyouSawamura <imbzss1234@gmail.com>
2013-11-25 14:22:10 +0900

    あざす
    よろしく

--- tokyo/module/functions.php
+++ tokyo/module/functions.php
@@ -8,14 +8,31 @@
 		DB Connection Function
 	*******************************/
 	function getConnection(){
+		
 		/**************************
 			DBroot = localhost
 			DBname = halcinema
 		**************************/
+		
 		$con = mysqli_connect('localhost','halcinema','halcinema');
 		mysqli_set_charset($con,'utf8');
 		mysqli_select_db($con,'halcinema');
+		
 		return $con;
+	}
+	
+	/*********************************
+		prefectures checked
+	*********************************/
+	
+	function prefectures($prefecture,$userPrefecture){
+		
+		if($prefecture == $userPrefecture){
+			return  "<option value='".$prefecture."' selected>".$prefecture."</option>";	
+		}else{
+			return   "<option value='".$prefecture."'>".$prefecture."</option>";
+		}
+		
 	}
 
 
--- tokyo/user/addUser.php
+++ tokyo/user/addUser.php
@@ -1,12 +1,10 @@
 <?PHP
 	require_once("../module/functions.php");
 
-	if(!(empty($_SESSION["userId"]) && empty($_SESSION["pass"]) && empty($_SESSION["mail"]) && empty($_SESSION["family"]) && empty($_SESSION["first"]) && empty($_SESSION["prefectures"]) && empty($_SESSION["address"]) && empty($_SESSION["birthday"])  && empty($_SESSION["tel"]))){
+	if(!(empty($_SESSION["userId"]) || empty($_SESSION["pass"]) || empty($_SESSION["mail"]) || empty($_SESSION["family"]) || empty($_SESSION["first"]) ||empty($_SESSION["prefectures"]) || empty($_SESSION["address"]) || empty($_SESSION["birthday"])  || empty($_SESSION["tel"]) || empty($_SESSION["mailMaga"]))){
 
 		//DB格納
-		$con = mysqli_connect('localhost','halcinema','halcinema');
-		mysqli_set_charset($con,'utf8');
-		mysqli_select_db($con,'halcinema');
+		$con=getConnection();
 		
 		$userId = mysqli_real_escape_string($con,$_SESSION["userId"]);
 		$pass = mysqli_real_escape_string($con,$_SESSION["pass"]);
@@ -17,12 +15,14 @@
 		$address = mysqli_real_escape_string($con,$_SESSION["address"]);
 		$tel = mysqli_real_escape_string($con,$_SESSION["tel"]);
 		$birthday = mysqli_real_escape_string($con,$_SESSION["birthday"]);
+		$mailMaga =  mysqli_real_escape_string($con,$_SESSION["mailMaga"]);
 	
 
-			$InsertSql = "INSERT INTO user_master(user_id, user_pass, user_mail, family_name, first_name,birthday, prefectures,user_address, user_tel) VALUES ('{$userId}','{$pass}','{$mail}','{$family}','{$first}','{$birthday}','{$prefectures}','{$address}','{$tel}')";
+			$InsertSql = "INSERT INTO user_master(user_id, user_pass, user_mail, family_name, first_name,birthday, prefectures,user_address, user_tel,mail_magazine_flag) VALUES ('{$userId}','{$pass}','{$mail}','{$family}','{$first}','{$birthday}','{$prefectures}','{$address}','{$tel}','{$mailMaga}')";
 
 			$InsertResult = mysqli_query($con,$InsertSql);
 			mysqli_close($con);
+			
 			if($InsertResult){
 				echo "完了";
 				header("Location:finish.php");
