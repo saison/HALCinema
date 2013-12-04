95ae0abcf2f4d71cd9f9bfa5c835c20d3757fd4f
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2013-12-04 16:49:12 +0900

    Revert "Merge pull request #72 from tak723sio/saisonScript"
    This reverts commit 54b03fb0d24b9397f0bb4bdb77e4ddfb431588c6, reversing
    changes made to 9fc3ee007e91fc2367cb8ecfcf028dbab3afa04d.

--- tokyo/module/js/script.js
+++ tokyo/module/js/script.js
@@ -1,6 +1,4 @@
 $(function(){
-	var name = "#subnav";
-
 	var footer = $("footer");
     var offset = footer.offset().top + footer.height();
     var win_h = $(window).height();
@@ -8,15 +6,8 @@ $(function(){
         footer.css({"position":"absolute", "bottom":"0"});
     }
 
-	$("#pagetop a,#subnav ul li a").click(function(){
+	$("#pagetop a").click(function(){
 		$('html,body').stop().animate({ scrollTop: $($(this).attr("href")).offset().top-50 },1500,'easeOutExpo');
 		return false;
-	});
-
-	//サイドバーをスクロールしてもついてくるように設定
-	menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))
-	$(window).scroll(function () {
-	    offset = menuYloc+$(document).scrollTop()+"px";
-	    $(name).animate({top:offset},{duration:900,queue:false});
 	});
 });
