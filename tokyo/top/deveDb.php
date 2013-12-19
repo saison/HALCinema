c4ea944d0f10481d262bc4adb7e0ac4490deac73
kix氏 <aiyuken2003@yahoo.co.jp>
2013-12-19 23:23:19 +0900

    開発者モード実�

---
+++ tokyo/top/deveDb.php
@@ -0,0 +1,13 @@
+<?php
+if(!empty($_COOKIE["deve_db"])){
+//ある
+	if($_COOKIE["deve_db"]=="xampp"){
+		setcookie('deve_db','manp',time()+60*6000);
+	}else{
+		setcookie('deve_db','xampp',time()+60*6000);
+	}
+}else{
+		setcookie('deve_db','xampp',time()+60*6000);
+}
+header("location:developer.php");
+?>
---
+++ tokyo/top/developer.php
@@ -0,0 +1,30 @@
+<?PHP
+$pageTitle = "開発者設定";
+require_once("../module/header.php");
+?>
+
+
+<div class="contentTitle" id="theatesTitle">
+<h2>開発者設定<span class="h2En">DeveloperSetup</span></h2>
+</div>
+<div id="mainContent">
+<p>開発者設定です。お使いの各PCの環境によって選択してください。</p>
+<h3>DB接続軽減モード</h3>
+<p>データベースの接続を早くするためのモードです。XAMPPなどの仮想環境では接続が重く、ページの読み込みが著しく遅くなる場合があります。遅い場合は軽減モードを設定してください。（XAMPPは軽減モード、MANPは通常モードをおすすめします）</p>
+<p>
+<?php 
+if(!empty($_COOKIE["deve_db"])){
+if($_COOKIE["deve_db"]=="xampp"){
+echo "現在は軽減モードです。<a href='deveDb.php'>切り替える</a>";
+}else{
+echo "現在は通常モードです。<a href='deveDb.php'>切り替える</a>";
+}
+}else{
+echo "設定されていません。<a href='deveDb.php'>設定する</a>";			
+}?></p>
+<p><a href="index.php">TOPページに戻る</a></p>
+</div>
+
+<?PHP
+require_once("../module/footer.php");
+?>
