a7d15d80c13474a369c6cce305c2ea00fc2cb9ce
kix氏 <aiyuken2003@yahoo.co.jp>
2014-02-21 02:16:34 +0900

    デザイン変更と不要ファイル削除

--- tokyo/top/deveDb.php
+++ tokyo/top/deveDb.php
@@ -2,12 +2,12 @@
 if(!empty($_COOKIE["deve_db"])){
 //ある
 	if($_COOKIE["deve_db"]=="xampp"){
-		setcookie('deve_db','mamp',time()+60*6000);
+		setcookie('deve_db','mamp',time()+60*6000*365);
 	}else{
-		setcookie('deve_db','xampp',time()+60*6000);
+		setcookie('deve_db','xampp',time()+60*6000*365);
 	}
 }else{
-		setcookie('deve_db','xampp',time()+60*6000);
+		setcookie('deve_db','xampp',time()+60*6000*365);
 }
 header("location:developer.php");
 ?>
