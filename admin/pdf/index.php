5b3a5f6b260c19a55a4acf0b8bddaad64a159264
Takuma Nishimura <tak.nishimura.0723@gmail.com>
2014-03-03 10:09:33 +0900

    なおした２

--- admin/pdf/index.php
+++ admin/pdf/index.php
@@ -13,24 +13,20 @@
       <h3>映画一覧</h3>
       <p>全ての映画を出力します</p>
       <a href="moviePdf.php" class="btn btn-danger btn-lg btn-block"><span class="glyphicon glyphicon-film"></span>映画一覧 PDF作成</a>
-      
+
       <h3>ユーザ一覧</h3>
       <p>全てのユーザを出力します</p>
       <a href="userPdf.php" class="btn btn-danger btn-lg btn-block"><span class="glyphicon glyphicon-user"></span>ユーザ一覧 PDF作成</a>
-      
+
       <h3>上映スケジュール一覧</h3>
       <p>全ての上映スケジュールを出力します</p>
       <a href="schedulePdf.php" class="btn btn-danger btn-lg btn-block"><span class="glyphicon glyphicon-tasks"></span>上映スケジュール一覧 PDF作成</a>
-      
+
       <h3>統計</h3>
       <p>HALCinemaの予約に関する統計を出力します</p>
-<<<<<<< HEAD
       <a href="statisticsPdf.php" class="btn btn-danger btn-lg btn-block"><span class="glyphicon glyphicon-align-left"></span>予約統計 PDF作成</a>
-=======
-      <a href="【アドレス】" class="btn btn-danger btn-lg btn-block"><span class="glyphicon glyphicon-align-left"></span>統計 PDF作成</a>
->>>>>>> master
     </div>
-    
+
     <div class="col-md-6">
       <h3>以下の項目は各ページにてPDF出力をしてください</h3>
       <ul>
@@ -38,7 +34,7 @@
         <li>各スケジュール詳細</li>
       </ul>
     </div>
-    
+
   </div>
 
 <?PHP
